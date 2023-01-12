44c8bb29228238830f82187600a8c094ea324bda
Setanna <joenhendrikhoelckhansen@gmail.com>
2023-01-12 11:27:24 +0100

    BuyPage Base No Data

---
+++ Hanseatic/ViewModel/BuyPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Android.Database;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanseatic.ViewModel;
+
+public partial class BuyPageViewModel : ObservableObject
+{
+    public productsViewModel()
+    {
+        products = new ObservableCollection<object>
+        {
+            new product
+            {
+                Name = "Beer",
+                Town = 62,
+                Buy = 50,
+                Sell = 47,
+                Ship = 0
+            }
+        };    
+    }
+
+    [ObservableProperty]
+    ObservableCollection<Object> products;
+}
---
+++ Hanseatic/ViewModel/products.cs
@@ -0,0 +1,11 @@
+﻿namespace Hanseatic.ViewModel
+{
+    internal class products
+    {
+        public string Name { get; set; }
+        public int Town { get; set; }
+        public int Buy { get; set; }
+        public int Sell { get; set; }
+        public int Ship { get; set; }
+    }
+}
